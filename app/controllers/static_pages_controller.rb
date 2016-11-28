02b1b3fe8e387952a2e6decf1630dd3ca45e020d
Sebastian Bottermann <sbottermann@web.de>
2016-11-28 11:08:27 +0100

    static pages controller corrected

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,6 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(5)
   	@featured_product = Product.first
-  	@seeded_product = Product.find(15).name
+  	@seeded_product = Product.find(12).name
   end
 end
