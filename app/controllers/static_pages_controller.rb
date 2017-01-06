70f32e1bf43a3a4923810a0428d8d99a2be95df7
Sebastian Bottermann <sbottermann@web.de>
2017-01-06 14:49:30 +0100

    5.3 Contact form created - Controller action and view defined, E-mail server Sendgrid installed and setted up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,15 @@ class StaticPagesController < ApplicationController
   	@products = Product.limit(5)
   	@featured_product = Product.first
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+        :to => 'sbottermann@web.de',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver_now
+  end
+
 end
