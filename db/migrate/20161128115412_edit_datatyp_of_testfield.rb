7ca0b4f8a11ef61d2e8b9fc09b45f1747778e802
Sebastian Bottermann <sbottermann@web.de>
2017-01-23 11:17:59 +0100

    migration file 20161128115412 modified for solving problems on heroku

--- db/migrate/20161128115412_edit_datatyp_of_testfield.rb
+++ db/migrate/20161128115412_edit_datatyp_of_testfield.rb
@@ -1,5 +1,5 @@
 class EditDatatypOfTestfield < ActiveRecord::Migration
   def change
-  	change_column :products, :testfield, :integer
+  	change_column :products, :testfield, 'integer USING CAST(column_name AS integer)'
   end
 end
