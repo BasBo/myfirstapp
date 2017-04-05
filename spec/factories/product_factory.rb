a57270768b586bdf2dbec0aa59e8616a7499f812
Sebastian Bottermann <sbottermann@web.de>
2017-04-05 12:43:47 +0200

    Specs for product model validations added

--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
 	factory :product do
 		name "race bike"
+		description "really fast"
+		colour "red"
+		image_url "test.jpg"
+		price 500
 	end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,4 +24,103 @@ describe Product do					#describe block (tells RSpec what we want to test, in th
 			expect(Product.new(description: "Nice bike")).not_to be_valid
 		end
 	end
+
+	# Specs of product model validations
+	# Spec of validates :presence
+	context "All input fields are filled in" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: 100)
+		end
+
+		it "Validation of presence should return no error" do
+			@product.valid?
+			@product.errors.full_messages.should_not include("can't be blank")
+		end
+	end 
+
+	context "One input field is blank" do
+		before do
+			@product = Product.new(name:"", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: 100)
+		end
+
+		it "Validation of presence should return error" do
+			@product.valid?
+			@product.errors.full_messages.should include("Name can't be blank")
+		end
+	end
+
+	context "All input fields are blank" do
+		before do
+			@product = Product.new(name:"", description:"", image_url:"", colour:"", price:"")
+		end
+
+		it "Validation of presence should return errors" do
+			@product.valid?
+			@product.errors.full_messages.should include("Name can't be blank", "Description can't be blank", "Image url can't be blank", "Colour can't be blank", "Price can't be blank")
+		end
+	end
+	
+	# Spec of validates :format
+	context "Entered image url contains .jpg extension" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: 100)
+		end
+
+		it "Validation of format should return no error" do
+			@product.valid?
+			@product.errors[:image_url].should_not include("is invalid")
+		end
+	end
+
+	context "Entered image url contains has missing .jpg extension" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter", colour: "test", price: 100)
+		end
+
+		it "Validation of format should return error" do
+			@product.valid?
+			@product.errors[:image_url].should include("is invalid")
+		end
+	end
+	# Spec of validates :numericality
+	context "Entered price is an integer" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: 100)
+		end
+
+		it "Validation of numericality should return no error" do
+			@product.valid?
+			@product.errors[:price].should_not include("is not a number")
+		end
+	end	
+	context "Entered price is not an integer" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: "one hundred")
+		end
+
+		it "Validation of numericality should return error" do
+			@product.valid?
+			@product.errors[:price].should include("is not a number")
+		end
+	end
+	context "Entered price is greater than zero" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: 100)
+		end
+
+		it "Validation of numericality should return no error" do
+			@product.valid?
+			@product.errors[:price].should_not include("must be greater than 0")
+		end
+	end		
+	context "Entered price is equal to zero" do
+		before do
+			@product = Product.new(name:"Testscooter", description:"For testing", image_url:"testscooter.jpg", colour: "test", price: 0)
+		end
+
+		it "Validation of numericality should return error" do
+			@product.valid?
+			@product.errors[:price].should include("must be greater than 0")
+		end
+	end		
 end
